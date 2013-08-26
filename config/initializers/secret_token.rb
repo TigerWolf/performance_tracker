24dc40b6581aa61a7403d3c53547b430a9ebde24
Kieran Andrews <hiddentiger@gmail.com>
2013-08-26 17:01:13 +0930

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PerformanceTracker::Application.config.secret_key_base = '58daeb532b5f36d4d0933c08f49d96942e4f2c737f5bb6359aab6b69a87bbf1324b40919c9a80cc9c419a2550d870aa3fbdb96885279e33aae74a0578895a367'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PerformanceTracker::Application.config.session_store :cookie_store, key: '_performance_tracker_session'
