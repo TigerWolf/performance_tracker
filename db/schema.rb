efa900e28f66330284882c53d4c6ac84b40de5bd
Kieran Andrews <hiddentiger@gmail.com>
2013-08-28 14:32:05 +0930

    Run migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20130828042936) do
     t.string   "cost"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "user_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
