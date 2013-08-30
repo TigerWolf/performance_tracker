472ba8a709b27b3f85e587746fe394e15c55764e
Kieran Andrews <hiddentiger@gmail.com>
2013-08-30 16:10:00 +0930

    Invalidate the cache when a portfolio is changed.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,9 @@
 class Portfolio < ActiveRecord::Base
+  after_update :invalidate_cache, :if => :campaigns_changed?
+
+  def invalidate_cache
+    cache = FileCache.new("portfolio_cost", "#{Rails.root}/cache", 1800,2)
+    cache.delete(self.id)
+
+  end
 end
