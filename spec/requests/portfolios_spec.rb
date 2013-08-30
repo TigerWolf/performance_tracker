b6c9c038cbf11c9cfb09de33a6cd0991fb27e9d5
Kieran Andrews <hiddentiger@gmail.com>
2013-08-30 16:42:49 +0930

    Add context.

--- spec/requests/portfolios_spec.rb
+++ spec/requests/portfolios_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe "Portfolios" do
   describe "GET /portfolios" do
-    it "redirects to login page" do
-      get portfolios_path
-      response.status.should be(302)
-      response.should redirect_to(login_prompt_path)
+
+    context 'when not logged in' do
+      it "redirects to login page" do
+        get portfolios_path
+        response.status.should be(302)
+        response.should redirect_to(login_prompt_path)
+      end
     end
+
   end
 end
