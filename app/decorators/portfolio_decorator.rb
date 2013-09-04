96c34e32feacf23cbf8588d8c33e6405f7c10b42
Kieran Andrews <hiddentiger@gmail.com>
2013-09-04 13:04:46 +0930

    Revise calculation and add comments.

--- app/decorators/portfolio_decorator.rb
+++ app/decorators/portfolio_decorator.rb
@@ -1,14 +1,16 @@
 class PortfolioDecorator < Draper::Decorator
   delegate_all
 
+  # Inputs: cost, monthly_budget
+  # Output: percentage of difference compared to target (e.g. positive means overspend)
   def difference
     if object.cost.present? && object.cost.to_i > 0 && object.montly_budget.to_f > 0 
-      days_in_month = Time.days_in_month(Time.now.month)
-      days_so_far_this_month = Date.yesterday.day
-      daily_budget = object.montly_budget.to_f/days_in_month
-      current_target = daily_budget*days_so_far_this_month
-      difference = current_target - object.cost.to_f
-      -(difference.to_f/current_target)*100
+      days_in_month           = Time.days_in_month(Time.now.month)
+      days_so_far_this_month  = Date.yesterday.day
+      daily_budget            = object.montly_budget.to_f / days_in_month
+      current_target          = daily_budget * days_so_far_this_month
+      difference              = current_target - object.cost.to_f
+      -((difference.to_f/current_target) * 100) # Calculate percentace and then inverse
     else
       0
     end
@@ -25,9 +27,8 @@ class PortfolioDecorator < Draper::Decorator
   end 
 
   def budget_left_per_day
-    #TODO - Need to ensure that float operations are done correctly.
     d = Date.yesterday
-    (object.cost.to_i - object.montly_budget.to_i)/(d.day.to_i - d.end_of_month.day.to_i)
+    (object.cost.to_f - object.montly_budget.to_f)/(d.day.to_i - d.end_of_month.day.to_i)
   end
 
 end
