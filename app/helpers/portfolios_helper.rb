6fdfb88e71878c738d59fec30c1360214b60fdd3
Kieran Andrews <hiddentiger@gmail.com>
2013-09-09 14:18:00 +0930

    Formatting and downcase.

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -5,12 +5,14 @@ module PortfoliosHelper
 
   # Only display exact and partial matches (2 chars needed)
   def self.search_sort(term, records)
+    term = term.downcase
+    record["text"] = record["text"].downcase
     exact_matches   = []
     partial_matches = []
     records.each do |record|
-      if term == record["text"].downcase
+      if term == record["text"]
         exact_matches << record
-      elsif  term.slice(0, 2) == record["text"].slice(0, 2)
+      elsif term.slice(0, 2) == record["text"].slice(0, 2)
         partial_matches << record
       end
     end
