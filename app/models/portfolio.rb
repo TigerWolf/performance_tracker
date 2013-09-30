c126839f67c6c965da299533fb670db616ee14c6
Kieran Andrews <hiddentiger@gmail.com>
2013-09-30 10:52:33 +0930

    Remove invalidation.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,14 +1,6 @@
 class Portfolio < ActiveRecord::Base
-  after_update :invalidate_cache, :if => :campaigns_changed?
 
   validates_numericality_of :montly_budget, :greater_than => 0
   validates :campaigns, :format => {:with => /^[0-9]+(,[0-9]+)*$/, :multiline => true }
 
-  def invalidate_cache
-    cache = FileCache.new("portfolio_cost", "#{Rails.root}/cache", 1800,2)
-    if cache.get(self.id).present? # If cache exists    
-      cache.delete(self.id)
-    end
-
-  end
 end
