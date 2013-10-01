ef6704cf55aea2ea62c590a6a0edef0a6459e4c9
Kieran Andrews <hiddentiger@gmail.com>
2013-10-01 13:52:35 +0930

    Formatting and comment doc.

--- app/services/portfolio_support/redis_query.rb
+++ app/services/portfolio_support/redis_query.rb
@@ -1,14 +1,22 @@
 module PortfolioSupport
     class RedisQuery
 
-      def self.refresh_redis_store(customer_id, current_user)
-        # Returns: redis namespace
-        namespaced = Redis::Namespace.new(customer_id, :redis => $redis)
-        unless namespaced.keys.present? # Check for existance of customer id namespace/cache
-          PortfolioSupport::AdwordsCampaignQuery.refresh_campaigns(customer_id, namespaced, current_user) # Cache expired, refresh
-        end
-        namespaced
+    ##
+    # This method will either refresh and get need data from google
+    # or return the previously saved data
+    # * *Args*    :
+    #   - +customer_id+  -> Google customer ID
+    #   - +current_user+ -> User object containing a token
+    # * *Returns* :
+    #   - A Redis object namespaced to the customer_id
+    #
+    def self.refresh_redis_store(customer_id, current_user)
+      namespaced = Redis::Namespace.new(customer_id, :redis => $redis)
+      unless namespaced.keys.present? # Check for existance of customer id namespace/cache
+        PortfolioSupport::AdwordsCampaignQuery.refresh_campaigns(customer_id, namespaced, current_user) # Cache expired, refresh
       end
-
+      namespaced
     end
+
   end
+end
