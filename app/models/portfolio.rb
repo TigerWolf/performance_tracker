57264037492b2b032dc70b1e80e942a8acf19643
Kieran Andrews <hiddentiger@gmail.com>
2013-10-01 14:37:29 +0930

    Call class method correctly.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -21,7 +21,7 @@ class Portfolio < ActiveRecord::Base
   end
 
   def aggregate_portfolio_cost(current_user)
-    campaign_cost = get_cost(PortfolioSupport::RedisQuery.refresh_redis_store(self.client_id, current_user), self)
+    campaign_cost = self.class.get_cost(PortfolioSupport::RedisQuery.refresh_redis_store(self.client_id, current_user), self)
     PortfoliosHelper.to_deci(campaign_cost)
   end
 
