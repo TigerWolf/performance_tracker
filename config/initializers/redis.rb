5be556661f12e5a2c96f7ed0e952e2461dd89afe
Kieran Andrews <hiddentiger@gmail.com>
2013-10-09 14:10:04 +1030

    Setup for engineyard.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,9 @@
-$redis = Redis::Namespace.new("performance_tracker", :redis => Redis.new)
+# Load the redis.yml configuration file
+redis_config = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env]
+
+# Connect to Redis using the redis_config host and port
+if redis_config
+  $redis_instance = Redis.new(host: redis_config['host'], port: redis_config['port'])
+end
+
+$redis = Redis::Namespace.new("performance_tracker", :redis => $redis_instance)
