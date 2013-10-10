0ef460a586697793c66240ec6df7e4546a3d2a46
Kieran Andrews <hiddentiger@gmail.com>
2013-10-10 13:04:45 +1030

    Testing for Auth.

--- app/services/ad_words_connection.rb
+++ app/services/ad_words_connection.rb
@@ -10,7 +10,7 @@ class AdWordsConnection
     return @api
   end
 
-  def self.create_adwords_api(token=nil, customer_id)
+  def self.create_adwords_api(token=nil, customer_id=nil)
     config_filename = File.join(Rails.root, 'config', 'adwords_api.yml')
     @api = AdwordsApi::Api.new(config_filename)
     # If we have an OAuth2 token in session we use the credentials from it.
