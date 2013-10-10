aa3610d828db0e930b58f2efb01ca787b4ef6425
Kieran Andrews <hiddentiger@gmail.com>
2013-10-10 13:52:58 +1030

    Remove old route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 PerformanceTracker::Application.routes.draw do
   get "portfolios/customer_list"
-  get "portfolios/report"  
+  get "portfolios/report"
   resources :portfolios
-
-  get "home/index"
 
   get "login/prompt"
   get "login/callback"
-  get "login/logout"  
+  get "login/logout"
 
   root "portfolios#report"
 
