31ba17d3578cc1a84b87c477f1eb3bb1eaa4a8f9
Kieran Andrews <hiddentiger@gmail.com>
2013-10-11 13:11:19 +1030

    Reload all the objects.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
   def report
     @portfolios = Portfolio.where(user_id: session[:user_id]).decorate
     Portfolio.refresh_costs(@portfolios, current_user)
-    @portfolios = @portfolios.reload
+    @portfolios = @portfolios.map(&:reload)
     @portfolios.sort!{ |a,b| b.difference.to_i.abs <=> a.difference.to_i.abs }
   end
 
