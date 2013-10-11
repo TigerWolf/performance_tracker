5b9747387885b1b7875bbb1df8ccf311d79c29e8
Kieran Andrews <hiddentiger@gmail.com>
2013-10-11 13:40:23 +1030

    Try to get rid of stale objects.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,6 +14,7 @@ class PortfoliosController < ApplicationController
   def report
     @portfolios = Portfolio.where(user_id: session[:user_id]).decorate
     @portfolios = Portfolio.refresh_costs(@portfolios, current_user)
+    @portfolios.map(&:reload)
     @portfolios.sort!{ |a,b| b.difference.to_i.abs <=> a.difference.to_i.abs }
   end
 
